f8268d38bcebbdde20ed7cc93c5e21e38c9952df
Marko1212 <shihonage7@gmail.com>
2020-06-18 10:58:49 +0200

    finished search and loader

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 import { PokemonsRoutingModule } from "./pokemons-routing.module";
 
 import { ListPokemonComponent } from "./list-pokemon.component";
 import { DetailPokemonComponent } from "./detail-pokemon.component";
+import { EditPokemonComponent } from "./edit-pokemon.component";
+import { PokemonFormComponent } from "./pokemon-form.component";
 import { PokemonSearchComponent } from "./search-pokemon.component";
+import { LoaderComponent } from "../loader.component";
 
 import { BorderCardDirective } from "./border-card.directive";
 import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 import { PokemonsService } from "./pokemons.service";
-
-import { FormsModule } from "@angular/forms";
-import { EditPokemonComponent } from "./edit-pokemon.component";
-import { PokemonFormComponent } from "./pokemon-form.component";
 
 @NgModule({
   imports: [CommonModule, FormsModule, PokemonsRoutingModule],
@@ -22,6 +22,7 @@ import { PokemonFormComponent } from "./pokemon-form.component";
     PokemonFormComponent,
     EditPokemonComponent,
     PokemonSearchComponent,
+    LoaderComponent,
     BorderCardDirective,
     PokemonTypeColorPipe,
   ],
